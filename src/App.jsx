bab76fe66ca1d761ed2416862428c8f4a2442612
Tanju Özer <tanjuozer64@gmail.com>
2024-04-19 14:30:19 +0200

    handle router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./page/RootLayout";
+import HomePage from "./page/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div>Appaa</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
